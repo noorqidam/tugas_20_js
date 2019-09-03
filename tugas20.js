e09d46d364ef60aa383fca3a936d4a4738cf1446
noorqidam <noorqidam@gmail.com>
2019-09-03 15:13:36 +0700

    Tugas 20 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas20.js
@@ -0,0 +1,6 @@
+function tugas20() {
+  var i ="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(i.match("are"));
+  console.log(i.match("words"));
+}
+tugas20();
